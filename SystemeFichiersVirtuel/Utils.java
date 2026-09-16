01a5ce7377265e2e920c7596ca567de376ba8d2d
BMaxime0 <maxime.beslon@gmail.com>
2026-09-16 10:32:07 +0200

    fin du tp 1

--- SystemeFichiersVirtuel/Utils.java
+++ SystemeFichiersVirtuel/Utils.java
@@ -3,12 +3,19 @@ public class Utils {
     public static int writeInt(byte[] memory, int offset, int value) {
         // TODO: Écrire les 4 octets de 'value' dans 'memory'
         // à partir de 'offset', en big-endian.
+
+        memory[offset + 3] = (byte) (value & 0xFF);
+        memory[offset + 2] = (byte) (value >> 8 & 0xFF);
+        memory[offset + 1] = (byte) (value >> 16 & 0xFF);
+        memory[offset] = (byte) (value >> 24 & 0xFF);
+
         return 4;
     }
 
     public static int readInt(byte[] memory, int offset) {
-        // TODO: Reconstituer le int sur 4 octets.
-        return 0;
+        int valeur = 0x00;
+        valeur = memory[offset + 3];
+        return valeur;
     }
 
     public static int writeShort(byte[] memory, int offset, short value) {
